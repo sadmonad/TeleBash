e3125ebe33ac75e5351541d7d81c1b722d6b2581
sadmonad <sadmonad@gmail.com>
2019-02-05 17:09:24 +0300

    Logging commands execution

--- telebash/core/handlers.py
+++ telebash/core/handlers.py
@@ -1,6 +1,14 @@
+import logging
+
 from .executors import DummyExecutor
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def command_hanlder(bot, update, user_data, cmd):
+    logger.info(f'Executing command: {cmd.cmd}')
     cmd_result = DummyExecutor.execute(cmd)
+    logger.info(f'Result: {cmd_result}')
     update.message.reply_text(cmd_result)
